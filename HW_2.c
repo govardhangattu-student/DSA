e79884cb4f90c51d3b85f00e45b8c1a4d8d0b3d5
govardhangattu-student <govardhang.eve24@chanakyauniversity.edu.in>
2025-08-18 15:26:38 +0530

    Add files via upload

---
+++ HW_2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a = 8;
+	int b = 4;
+	printf("%d\n",a+b);
+	printf("%d\n",a-b);
+	printf("%d\n",a*b);
+	printf("%d\n",a/b);
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		printf("2*%d=%d\n",i,2*i);
+	}
+}
+	
+	
+
